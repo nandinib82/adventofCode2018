18e8f44bcef6bbc8e7c82972ac571dcecadb7e3c
Nandini Bhattacharya <nandini.bhattacharya@gmail.com>
2018-12-01 18:28:54 -0800

    Problem 1 part 2

--- Day1.js
+++ Day1.js
@@ -4,19 +4,54 @@ function readFile(fileName) {
     return fs.readFileSync(fileName, 'utf8');
 }
 
-function findFrequency(textBuffer) {
-    const arr = textBuffer.split('\n').filter(line => {
+function parseFile(textBuffer)
+{
+    const elements = textBuffer.split('\n').filter(line => {
         return line.length > 0;
     });
 
+    return elements;
+}
+
+function findFrequency(frequencies) {
+    
     let frequency = 0;
-    arr.forEach(element => {
+    for(element of frequencies)
+    {            
         frequency = frequency + parseInt(element);
-    });
+    }
+    return frequency;
+}
+
+function findFirstRepeatingFrequency(frequencies)
+{
+    let frequency = 0;
+    let repeatFrequencyTest = new Set();
+    let foundDuplicate = false;
+    while(!foundDuplicate)
+    {
+        for(element of frequencies)
+        {            
+            frequency = frequency + parseInt(element);
+            if (repeatFrequencyTest.has(frequency))
+            {
+                foundDuplicate = true;
+                break;
+            }
+            else
+            {
+                repeatFrequencyTest.add(frequency);
+            }
+        }
+    }
 
     return frequency;
 }
 
 const textFileBuffer = readFile("./input.txt");
-const frequency = findFrequency(textFileBuffer);
-console.log(frequency);
+const frequencies = parseFile(textFileBuffer);
+const frequency = findFrequency(frequencies);
+console.log("Frequency :" + frequency);
+
+const duplicatedFrequency = findFirstRepeatingFrequency(frequencies);
+console.log("Duplicate frequency :" + duplicatedFrequency);
